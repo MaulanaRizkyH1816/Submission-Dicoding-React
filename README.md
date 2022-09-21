07014c32b1d2c67bd94aab3a46c044c4a7f07920
MaulanaRizkyH1816 <maulana.16@students.amikom.ac.id>
2022-09-21 13:49:37 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Dicoding Submission - Membangun Aplikasi Catatan Menggunakan React
+### [+] Preview
+<p style="text-align: center">
+   <img src="https://i.postimg.cc/Vv16Ykyz/Screenshot-2022-07-03-at-01-50-01-My-Personal-Notes.png" width="auto" height="400" />
+</p>
---
+++ src/components/NoteInput.js
@@ -0,0 +1,66 @@
+import { Component } from 'react';
+import autoBind from 'auto-bind';
+
+class NoteInput extends Component {
+   constructor(props) {
+      super(props);
+
+      this.state = {
+         title: '',
+         body: '',
+         limit: 50,
+      }
+
+      autoBind(this);
+   }
+
+   onTitleChangeHandler(event) {
+      const limit = 50;
+
+      this.setState((prevState) => {
+         return {
+            ...prevState,
+            title: event.target.value.slice(0, limit),
+            limit: limit - event.target.value.slice(0, limit).length,
+         }
+      });
+   }
+
+   onBodyChangeHandler(event) {
+      this.setState((prevState) => {
+         return {
+            ...prevState,
+            body: event.target.value,
+         }
+      });
+   }
+
+   resetFormInput() {
+      this.setState((prevState) => {
+         return {
+            title: prevState.title = '',
+            body: prevState.body = '',
+            limit: prevState.limit = 50,
+         }
+      });
+   }
+
+   onSubmitEventHandler(event) {
+      event.preventDefault();
+      this.props.addNote(this.state);
+      this.resetFormInput();
+   }
+
+   render() {
+      return (
+         <form className="note__form" onSubmit={this.onSubmitEventHandler}>
+            <div className="note__form__limit-info">Sisa karakter <span>{this.state.limit}</span></div>
+            <input type="text" placeholder="Title" required value={this.state.title} onChange={this.onTitleChangeHandler} />
+            <textarea placeholder="Write your notes..." rows="5" required value={this.state.body} onChange={this.onBodyChangeHandler}></textarea>
+            <button type="submit" className="btn btn--primary">Simpan</button>
+         </form>
+      )
+   }
+}
+
+export default NoteInput;
